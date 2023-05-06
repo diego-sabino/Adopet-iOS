216c76dc185efb5826028d233975b6c729e68848
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-06 07:32:12 -0300

    Implement initial unofficial login screen layout (v1)

--- Adopet/Adopet/ContentView.swift
+++ Adopet/Adopet/ContentView.swift
@@ -10,14 +10,62 @@ import Inject
 
 struct ContentView: View {
     @ObservedObject private var iO = Inject.observer
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("BackgroundDefault")
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("MainLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 20)
+                    
+    
+                VStack(spacing: 10) {
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 20)
+                }
+                .shadow(radius: 5)
+                .padding(.horizontal, 20)
+                
+                Spacer().frame(height: 125)
+                
+                Button(action: {}) {
+                    Text("Login")
+                        .frame(width: 300, height: 50)
+                        .background(Color("MainButton"))
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                }
+                
+                Button(action: {}) {
+                    Text("Don't have an account? Sign Up")
+                        .frame(width: 300, height: 50)
+                        .foregroundColor(Color("MainButton"))
+                        .font(.callout)
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
         .enableInjection()
     }
 }
