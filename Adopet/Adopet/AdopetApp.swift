7f530f93cd4120004c43bedd7df1b9e0fd3e474d
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-06 04:52:00 -0300

    create inject environment

---
+++ Adopet/Adopet/AdopetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdopetApp.swift
+//  Adopet
+//
+//  Created by Diego Sabino on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AdopetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
