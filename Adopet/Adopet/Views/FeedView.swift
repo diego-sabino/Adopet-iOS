8ace9349f5f7e5f68e3a56799605e9580d06035e
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-06 23:54:27 -0300

    feat: add pet card view

--- Adopet/Adopet/Views/FeedView.swift
+++ Adopet/Adopet/Views/FeedView.swift
@@ -17,22 +17,23 @@ struct FeedView: View {
     private let apiManager = APIManager()
     
     var body: some View {
-        ZStack {
-            List(data, id: \._id) { item in
-                Text(item.name)
-                AsyncImage(url: URL(string: item.photos[0])) { image in
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit) } placeholder: { Color.gray }
-                    .listRowInsets(EdgeInsets())
-                    .cornerRadius(10)
+            ZStack {
+                List(data, id: \._id) { item in
+                    PetCardView(pet: item)
+                        .background(Color("BackgroundCard"))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.vertical)
+                        .listRowInsets(EdgeInsets())
+                        .listRowSeparator(.hidden)
+                }
+                
+                .scrollContentBackground(.hidden)
+                .onAppear { fetchData() }
             }
-            .scrollContentBackground(.hidden)
-            .onAppear { fetchData() }
+            .navigationBarBackButtonHidden(true)
+            .enableInjection()
         }
-        .navigationBarBackButtonHidden(true)
-        .enableInjection()
-    }
     
     func fetchData() {
         apiManager.fetchData { result in
