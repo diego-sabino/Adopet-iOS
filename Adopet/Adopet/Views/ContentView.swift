bf613bb2e29d0583823e017fc657971056545187
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-07 01:07:15 -0300

    feat: Change list style to "PlainListStyle"

---
+++ Adopet/Adopet/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adopet
+//
+//  Created by Diego Sabino on 06/05/23.
+//
+
+import SwiftUI
+import Inject
+
+struct ContentView: View {
+    @ObservedObject private var iO = Inject.observer
+    
+    
+    var body: some View {
+        NavigationView {
+            LoginView()   
+        }
+        .enableInjection()
+    }
+}
