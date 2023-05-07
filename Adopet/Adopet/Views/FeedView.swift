4a826f6f379b368b75e254e8170da78ffb5a7bfc
Diego-Sabino <dbrgamei5@gmail.com>
2023-05-06 23:06:13 -0300

    feat: Initial feed screen

--- Adopet/Adopet/Views/FeedView.swift
+++ Adopet/Adopet/Views/FeedView.swift
@@ -11,13 +11,36 @@ import Inject
 struct FeedView: View {
     @ObservedObject private var iO = Inject.observer
     
+    @State var email: String = ""
+    @State var password: String = ""
+    @State private var data: [Pet] = []
+    private let apiManager = APIManager()
+    
     var body: some View {
-           NavigationView {
-               VStack {
-                   Text("Hello, World!")
-               }
-           }
-           .navigationBarBackButtonHidden(true)
-           .enableInjection()
-       }
-   }
+        ZStack {
+            List(data, id: \._id) { item in
+                Text(item.name)
+                AsyncImage(url: URL(string: item.photos[0])) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit) } placeholder: { Color.gray }
+                    .listRowInsets(EdgeInsets())
+                    .cornerRadius(10)
+            }
+            .scrollContentBackground(.hidden)
+            .onAppear { fetchData() }
+        }
+        .navigationBarBackButtonHidden(true)
+        .enableInjection()
+    }
+    
+    func fetchData() {
+        apiManager.fetchData { result in
+            if let result = result {
+                DispatchQueue.main.async {
+                    self.data = result
+                }
+            }
+        }
+    }
+}
